fc01b65ec7c3aaf5bc026c27292d213483743cb7
Alejandro González <andresg.vergara04@gmail.com>
2024-10-28 12:33:27 -0300

    commit final
    se intentó

--- evaAutores/projectAutores/appAutores/forms.py
+++ evaAutores/projectAutores/appAutores/forms.py
@@ -4,3 +4,26 @@ class FormAutor(forms.ModelForm):
     class Meta:
         model = Autor
         fields = '__all__'
+        
+        def clean(self):
+            user_clean_data = super().clean()
+
+            inputNombre = user_clean_data['nombre']
+            if len(inputNombre) > 20:
+                raise forms.ValidationError("El largo máximo del nombre son 20 caracteres..")
+            elif len(inputNombre) < 3:
+                raise forms.ValidationError("El mínimo de este campo son 3 caracteres..")
+            
+            inputApellido = user_clean_data['apellido']
+            if len(inputApellido) > 20:
+                raise forms.ValidationError("El largo máximo del apellido son 20 caracteres..")
+            elif len(inputApellido) < 3:
+                raise forms.ValidationError("El mínimo de este campo son 3 caracteres..")
+            
+            inputCorreo = user_clean_data['correo']
+            if len(inputCorreo) > 50:
+                raise forms.ValidationError("El largo máximo del correo son 50 caracteres..")
+            elif len(inputCorreo) < 3:
+                raise forms.ValidationError("El mínimo de este campo son 3 caracteres..")
+            
+            return inputNombre, inputApellido, inputCorreo
